127df338b9d93c0748cbc75428fd076d66f72c0c
Dmizuta <dani.mizuta@gmail.com>
2025-07-16 10:44:49 -0300

    feat: sidebar

--- script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  fetch('sidebar.html')
-    .then(response => response.text())
-    .then(html => {
-      document.querySelectorAll('.sidebar').forEach(el => el.outerHTML = html);
-    });
-});
