2dabcb88fe9d119e82ca9ab34dd47ed00110a9ae
Dani Mizuta <dani.mizuta@gmail.com>
2025-07-17 22:20:13 -0300

    feat:refactoring

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
-
-  fetch('sidebar.html')
-    .then(res => res.text())
-    .then(html => {
-      document.getElementById('sidebar-placeholder').innerHTML = html;
-    });
-
+fetch('/components/sidebar.html')
+  .then(res => res.text())
+  .then(html => {
+    document.getElementById('sidebar-placeholder').innerHTML = html;
+  });
