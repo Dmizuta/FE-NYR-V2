cb2b7adb3de505ba3994c68150c9dbbcd1199039
Dani Mizuta <dani.mizuta@gmail.com>
2025-07-24 22:41:23 -0300

    feat: my first order

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 
-//window.BACKEND_URL = "https://be-nyr-v2.vercel.app"; // 👉 Produção (Vercel)
+window.BACKEND_URL = "https://be-nyr-v2.vercel.app"; // 👉 Produção (Vercel)
 
-window.BACKEND_URL = "http://localhost:3000";
+//window.BACKEND_URL = "http://localhost:3000";
 
 const URL_API = window.BACKEND_URL;
 
